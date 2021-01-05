2a8c5ad24b852efe74d2169886c641e54f6263b6
Csaba Huzián <csaba.huzian@stud.uni-corvinus.hu>
2021-01-05 22:48:39 +0100

    2. Osztályok fájlbeolvasáshoz

---
+++ ShipmentTracking/ShipmentTracking/Entities/CustomerOrders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentTracking.Entities
+{
+    public class CustomerOrders
+    {
+        public int CustomerCode { get; set; }
+        public int OrderNumber { get; set; }
+
+    }
+}
---
+++ ShipmentTracking/ShipmentTracking/Entities/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipmentTracking.Entities
+{
+    public class Warehouse
+    {
+        public int OrderNumber { get; set; }
+        public int PackageNumber { get; set; }
+        public int PickUpDate { get; set; }
+    }
+}
