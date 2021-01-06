19fef30f683c338dd6a907137c3020b6adb0a6fc
Csaba Huzián <csaba.huzian@stud.uni-corvinus.hu>
2021-01-06 20:34:02 +0100

    7. Form1 design kialakítása, csv fájl user általi kiválasztása.

--- ShipmentTracking/ShipmentTracking/Form1.Designer.cs
+++ ShipmentTracking/ShipmentTracking/Form1.Designer.cs
@@ -32,33 +32,188 @@ namespace ShipmentTracking
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBoxWarehouse = new System.Windows.Forms.TextBox();
+            this.buttonWarehouse = new System.Windows.Forms.Button();
+            this.textBoxForwarder = new System.Windows.Forms.TextBox();
+            this.buttonForwarder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxExport = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Menu;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(132, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(41, 164);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(373, 542);
+            this.dataGridView1.Size = new System.Drawing.Size(669, 376);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxWarehouse
+            // 
+            this.textBoxWarehouse.Enabled = false;
+            this.textBoxWarehouse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxWarehouse.Location = new System.Drawing.Point(172, 36);
+            this.textBoxWarehouse.Name = "textBoxWarehouse";
+            this.textBoxWarehouse.Size = new System.Drawing.Size(238, 26);
+            this.textBoxWarehouse.TabIndex = 2;
+            // 
+            // buttonWarehouse
+            // 
+            this.buttonWarehouse.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonWarehouse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonWarehouse.Location = new System.Drawing.Point(416, 36);
+            this.buttonWarehouse.Name = "buttonWarehouse";
+            this.buttonWarehouse.Size = new System.Drawing.Size(75, 26);
+            this.buttonWarehouse.TabIndex = 4;
+            this.buttonWarehouse.Text = "Browse";
+            this.buttonWarehouse.UseVisualStyleBackColor = false;
+            this.buttonWarehouse.Click += new System.EventHandler(this.buttonWarehouse_Click);
+            // 
+            // textBoxForwarder
+            // 
+            this.textBoxForwarder.Enabled = false;
+            this.textBoxForwarder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxForwarder.Location = new System.Drawing.Point(172, 68);
+            this.textBoxForwarder.Name = "textBoxForwarder";
+            this.textBoxForwarder.Size = new System.Drawing.Size(238, 26);
+            this.textBoxForwarder.TabIndex = 5;
+            // 
+            // buttonForwarder
+            // 
+            this.buttonForwarder.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonForwarder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonForwarder.Location = new System.Drawing.Point(416, 71);
+            this.buttonForwarder.Name = "buttonForwarder";
+            this.buttonForwarder.Size = new System.Drawing.Size(75, 26);
+            this.buttonForwarder.TabIndex = 6;
+            this.buttonForwarder.Text = "Browse";
+            this.buttonForwarder.UseVisualStyleBackColor = false;
+            this.buttonForwarder.Click += new System.EventHandler(this.buttonForwarder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(37, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Warehouse file";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(37, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Forwarder file";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(37, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Export";
+            // 
+            // textBoxExport
+            // 
+            this.textBoxExport.Enabled = false;
+            this.textBoxExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxExport.Location = new System.Drawing.Point(172, 100);
+            this.textBoxExport.Name = "textBoxExport";
+            this.textBoxExport.Size = new System.Drawing.Size(238, 26);
+            this.textBoxExport.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PeachPuff;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(416, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.Location = new System.Drawing.Point(716, 164);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 26);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PeachPuff;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(716, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 26);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStop.Location = new System.Drawing.Point(716, 196);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 26);
+            this.buttonStop.TabIndex = 14;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(574, 44);
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(35, 126);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 35);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Teljesített megrendelések";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 578);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(846, 624);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxExport);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonForwarder);
+            this.Controls.Add(this.textBoxForwarder);
+            this.Controls.Add(this.buttonWarehouse);
+            this.Controls.Add(this.textBoxWarehouse);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -72,6 +227,18 @@ namespace ShipmentTracking
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBoxWarehouse;
+        private System.Windows.Forms.Button buttonWarehouse;
+        private System.Windows.Forms.TextBox textBoxForwarder;
+        private System.Windows.Forms.Button buttonForwarder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxExport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonStop;
         private System.Windows.Forms.Label label1;
     }
 }
--- ShipmentTracking/ShipmentTracking/Form1.cs
+++ ShipmentTracking/ShipmentTracking/Form1.cs
@@ -17,27 +17,21 @@ namespace ShipmentTracking
         List<Forwarder> ShipmentStatus = new List<Forwarder>();
         List<Warehouse> ShipmentPacking = new List<Warehouse>();
 
-        //public int ido = 0;
 
         public Form1()
         {
             InitializeComponent();
 
             //label1.Text = "0";
-            timer1.Interval = 5000;
-            timer1.Start();
-            timer1.Tick += Timer1_Tick;
+
 
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            //ido++;
-            //label1.Text = ido.ToString();
 
-          
-            ShipmentStatus = GetShipmentStatus(@"C:\Temp\forwarder.csv");
-            ShipmentPacking = GetShipmentPacking(@"C:\Temp\warehouse.csv");
+            ShipmentStatus = GetShipmentStatus(textBoxForwarder.Text);
+            ShipmentPacking = GetShipmentPacking(textBoxWarehouse.Text);
 
             for (int z = ShipmentPacking.Count - 1; z >= 0; z--)
             {
@@ -87,8 +81,8 @@ namespace ShipmentTracking
 
         public List<Forwarder> GetShipmentStatus(string csvpath)
         {
-            List<Forwarder> forwarder = new List<Forwarder>();
 
+            List<Forwarder> forwarder = new List<Forwarder>();
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
@@ -103,6 +97,63 @@ namespace ShipmentTracking
                 }
             }
             return forwarder;
+        }
+
+        private void buttonWarehouse_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    textBoxWarehouse.Text = filePath;
+                }
+            }
+        }
+
+        private void buttonForwarder_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    textBoxForwarder.Text = filePath;
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxForwarder.Text) && string.IsNullOrEmpty(textBoxWarehouse.Text))
+            {
+                MessageBox.Show("Hiányzó csv fájl elérés!");
+                return;
+            }
+
+            timer1.Interval = 2000;
+            timer1.Start();
+            timer1.Tick += Timer1_Tick;
+  
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
